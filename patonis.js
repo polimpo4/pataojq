ba8805dbfd21a871a30686174377a0a713913420
polimpo4 <polimpo4@hotmail.com>
2025-08-31 16:38:24 +0000

    Update patonis.js

--- patonis.js
+++ patonis.js
@@ -1,10 +1,10 @@
 /*!
- * jQuery JavaScript Library v3.7.0
- * https://jquery.com/
+ * patonis JavaScript Library v3.7.0
+ * https://patao.cloud/
  *
  * Copyright OpenJS Foundation and other contributors
  * Released under the MIT license
- * https://jquery.org/license
+ * 
  *
  * Date: 2023-05-11T18:29Z
  */
